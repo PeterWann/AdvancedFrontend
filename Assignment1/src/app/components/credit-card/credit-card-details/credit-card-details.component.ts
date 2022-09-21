e33c636542055253ff82e0d5605f4f0f2a7b0852
WannTheMan <jisskiss@gmail.com>
2022-09-22 00:49:45 +0200

    Card Details (still missing transactions)

--- Assignment1/src/app/components/credit-card/credit-card-details/credit-card-details.component.ts
+++ Assignment1/src/app/components/credit-card/credit-card-details/credit-card-details.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { CreditCard } from 'src/app/models/credit-card.interface';
+import { CreditCardService } from 'src/app/services/credit-card.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-credit-card-details',
   templateUrl: './credit-card-details.component.html',
-  styleUrls: ['./credit-card-details.component.scss']
+  styleUrls: ['./credit-card-details.component.scss'],
 })
 export class CreditCardDetailsComponent implements OnInit {
+  cardDetails$!: CreditCard;
+  id!: number;
 
-  constructor() { }
+  constructor(
+    private cardService: CreditCardService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.cardService.getCreditCard(this.id).subscribe((cardDetails) => {
+      this.cardDetails$ = cardDetails;
+    });
   }
 
+  deleteCreditCard() {
+    this.cardService.deleteCreditCard(this.id).subscribe(() => {
+      console.log('User Deleted');
+      this.router.navigate(['']);
+    });
+  }
 }
